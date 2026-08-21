b54d0089418de8ea1b8de871ff59ba2d2e87337d
Addisu Sheko <adisusheko@gmail.com>
2026-08-21 08:48:45 -0400

    update

--- TmsApi.Infrastructure/obj/Debug/net10.0/TmsApi.Infrastructure.AssemblyInfo.cs
+++ TmsApi.Infrastructure/obj/Debug/net10.0/TmsApi.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70fa216a9ff100e6427b62168d18ef74652232ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c58805a78f452c8cba980e71d69259408ccc8bf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TmsApi.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
